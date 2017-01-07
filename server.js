e634c08a2d1af35d7b5736b2a8ac29811fcf36a3
Joe Karlsson <josephcarlson1@gmail.com>
2017-01-07 13:20:37 -1000

    flexible port and host

--- server.js
+++ server.js
@@ -3,9 +3,13 @@ const send = require('./routes/send');
 
 const app = express();
 
+const isDeveloping = process.env.NODE_ENV !== 'production';
+const port = isDeveloping ? 3000 : process.env.PORT;
+const host = isDeveloping ? 'localhost' :  '0.0.0.0';
+
 app.use(express.static(`${__dirname}/public`));
 app.use('/send', send);
 
-app.listen(3000,function(){
+app.listen(port,function(){
   console.log("Express Started on Port 3000");
 });
