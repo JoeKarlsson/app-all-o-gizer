145be18b25232d5c56c65d2854ea1e65a6a3cedd
Joe Karlsson <josephcarlson1@gmail.com>
2017-01-07 16:33:10 -1000

    added info about the site

--- messages.js
+++ messages.js
@@ -1,43 +1,43 @@
 const messages = [
   {
     subject: 'Sorry I fucked up',
-    body: 'I am so sorry that I missed that super important event that you planned. I love you so much and I know this was a big deal. I\'ll make it up up to you someday, I promise.'
+    body: 'I am so sorry that I missed that super important event that you planned. I love you so much and I know this was a big deal. I\'ll make it up up to you someday, I promise. \n\nEmail sent by appallogizer.com'
   }, {
     subject: 'So sorry I missed our anniversary',
-    body: 'Sorry babe, that I missed our anniversary. I love you so much and I know that anniversaries are a big deal. You are the sweetest, kindest person that I know. Please forgive me.'
+    body: 'Sorry babe, that I missed our anniversary. I love you so much and I know that anniversaries are a big deal. You are the sweetest, kindest person that I know. Please forgive me. \n\nEmail sent by appallogizer.com'
   }, {
     subject: 'Sorry about peeing the bed',
-    body: 'Sorry that I peed the bed. I have always had a bed wetting problem, and I can\'t help it. I promise to wash the sheets next time.'
+    body: 'Sorry that I peed the bed. I have always had a bed wetting problem, and I can\'t help it. I promise to wash the sheets next time. \n\nEmail sent by appallogizer.com'
   }, {
     subject: 'Sorry I was Late to work',
-    body: 'I was a little late to work today. This morning I was being chased by a rabid dog, and then I got in a terrible car accident (don\'t worry, I\'m totally okay). In the future, I\'ll be sure to be in the office in time.'
+    body: 'I was a little late to work today. This morning I was being chased by a rabid dog, and then I got in a terrible car accident (don\'t worry, I\'m totally okay). In the future, I\'ll be sure to be in the office in time.  \n\nEmail sent by appallogizer.com'
   }, {
     subject: 'Sorry about the murder',
-    body: 'It was a total freak accident that I murdered that guy last week. I hope that you will find it in your heart to forgive me for my heinous crime against humanity.'
+    body: 'It was a total freak accident that I murdered that guy last week. I hope that you will find it in your heart to forgive me for my heinous crime against humanity.  \n\nEmail sent by appallogizer.com'
   }, {
     subject: 'Sorry I missed your funeral',
-    body: 'I know you died a couple of days ago, and I wish I could have made it, but there was this new show on Netflix, and I wanted to be a part of the cultural conversation about it, so I had to binged it. Anyways, thanks for everything, I\'ll see you in hell. '
+    body: 'I know you died a couple of days ago, and I wish I could have made it, but there was this new show on Netflix, and I wanted to be a part of the cultural conversation about it, so I had to binged it. Anyways, thanks for everything, I\'ll see you in hell. \n\nEmail sent by appallogizer.com'
   }, {
     subject: 'Sorry for tricking your kids into trafficing drugs for me',
-    body: 'I didn\'t think it was a big deal or whatever, but I can totally understand why you would be pissed at me for accidently tricking your kids into trafficing those drugs for me and my boys. I thought for sure that they wouldn\'t be caught, because, you know, they are kids and all. But, boy was I wrong. Anyways, thanks for being such a great pal. Love you. Bye now.'
+    body: 'I didn\'t think it was a big deal or whatever, but I can totally understand why you would be pissed at me for accidently tricking your kids into trafficing those drugs for me and my boys. I thought for sure that they wouldn\'t be caught, because, you know, they are kids and all. But, boy was I wrong. Anyways, thanks for being such a great pal. Love you. Bye now. \n\nEmail sent by appallogizer.com'
   }, {
     subject: 'Sorry I pooped the bed',
-    body: 'It was a total accident and I will totally buy you a new bed.'
+    body: 'It was a total accident and I will totally buy you a new bed. \n\n Email sent by appallogizer.com'
   }, {
     subject: 'Sorry I blew up your living room',
-    body: 'Sorry I blew up your living room. I didn\'t realize that those crazy explosives I bought from Mexicio would have made such a massive explosion. Best of luck with all of the repairs.'
+    body: 'Sorry I blew up your living room. I didn\'t realize that those crazy explosives I bought from Mexico would have made such a massive explosion. Best of luck with all of the repairs. \n\nEmail sent by appallogizer.com'
   }, {
     subject: 'Sorry I accidently got you addicted to Meth',
-    body: 'I thought it would be funny if I got you addicited to methamphetamine, but I see now that this was not a funny prank to pull on someone like you. Best of luck in rehab, let\'s grab drinks when you get out.'
+    body: 'I thought it would be funny if I got you addicited to methamphetamine, but I see now that this was not a funny prank to pull on someone like you. Best of luck in rehab, let\'s grab drinks when you get out. \n\nEmail sent by appallogizer.com'
   }, {
     subject: 'Sorry about accidently stabbing you in the dick',
-    body: 'Sorry about accidently stabbing you in the dick. I hope that you don\'t totally lose your penis after that freak accident. Let get drinks when you get out of the hospital. Cheers mate!'
+    body: 'Sorry about accidently stabbing you in the dick. I hope that you don\'t totally lose your penis after that freak accident. Let get drinks when you get out of the hospital. Cheers mate! \n\nEmail sent by appallogizer.com'
   }, {
     subject: 'Sorry about flirting with your Mom/Dad',
-    body: 'Sorry about flirting with your Mom and Dad at the funeral. I had no idea that they were your parents, and if I did, I totally never would have done it.'
+    body: 'Sorry about flirting with your Mom and Dad at the funeral. I had no idea that they were your parents, and if I did, I totally never would have done it. \n\nEmail sent by appallogizer.com'
   }, {
     subject: 'Sorry I missed your shitty band\'s show',
-    body: 'Your band is great and all, but I totally missed your show when I took a nap instead. I\'ll be sure to catch you next time bro!'
+    body: 'Your band is great and all, but I totally missed your show when I took a nap instead. I\'ll be sure to catch you next time bro! \n\nEmail sent by appallogizer.com'
   }
 ];
 
