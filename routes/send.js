93a5ac7199a5c5bd2bf1df46d5d6fce296310922
Joe Karlsson <joe.carlson@callmejoe.net>
2019-11-12 16:02:01 -0600

    fixed mailer.

--- routes/send.js
+++ routes/send.js
@@ -1,46 +1,44 @@
-const express = require('express');
+const express = require("express");
 const nodemailer = require("nodemailer");
-const messages = require('../messages');
+const messages = require("../messages");
 const router = express.Router();
 
 /*
   Here we are configuring our SMTP Server details.
   STMP is mail server which is responsible for sending and recieving email.
 */
-const smtpTransport = nodemailer.createTransport("SMTP",{
-    host: 'smtp.gmail.com',
-    port: 587,
-    auth: {
-        user: process.env.email,
-        pass: process.env.pass,
-    },
-    tls: {rejectUnauthorized: false},
-    debug: true,
+
+const smtpTransport = nodemailer.createTransport({
+  host: "smtp.gmail.com",
+  secure: false,
+  auth: {
+    user: process.env.email,
+    pass: process.env.pass
+  },
+  debug: true
 });
 
-const randomMessage = (messagesArr) => {
-  return messagesArr[Math.floor(Math.random()*messagesArr.length)];
-}
+const randomMessage = messagesArr => {
+  return messagesArr[Math.floor(Math.random() * messagesArr.length)];
+};
 
-router.route('/')
-  .get(function(req,res){
+router.route("/").get(function(req, res) {
+  let randMsg = randomMessage(messages);
 
-    let randMsg = randomMessage(messages);
-
-    let mailOptions={
-      to : req.query.to,
-      subject : randMsg.subject,
-      text : randMsg.body,
+  let mailOptions = {
+    to: req.query.to,
+    subject: randMsg.subject,
+    text: randMsg.body
+  };
+  console.log(mailOptions);
+  smtpTransport.sendMail(mailOptions, function(error, response) {
+    if (error) {
+      console.log(error);
+      res.end("error");
+    } else {
+      console.log("Message sent: " + response.message);
+      res.end("sent");
     }
-    console.log(mailOptions);
-    smtpTransport.sendMail(mailOptions, function(error, response){
-      if(error){
-        console.log(error);
-        res.end("error");
-      } else {
-        console.log("Message sent: " + response.message);
-        res.end("sent");
-      }
   });
 });
 
